0448313697b12db38c4678f56147ce54f7698f7c
Travis Valentine <tvalent2@gmualumni.org>
2012-01-07 23:43:39 -0500

    Remove mobile mentions for now.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,23 +3,23 @@ class ApplicationController < ActionController::Base
   helper :all
   layout 'application'
 
-  before_filter :prepare_for_mobile
+  #before_filter :prepare_for_mobile
 
   private
 
-  def mobile_device?
-    if session[:mobile_param]
-      session[:mobile_param] == "1"
-    else
-      request.user_agent =~ /Mobile|webOS/
-    end
-  end
-  helper_method :mobile_device?
+  #def mobile_device?
+   # if session[:mobile_param]
+    #  session[:mobile_param] == "1"
+    #else
+     # request.user_agent =~ /Mobile|webOS/
+    #end
+  #end
+  #helper_method :mobile_device?
 
-  def prepare_for_mobile
-    session[:mobile_param] = params[:mobile] if params[:mobile]
-    request.format = :mobile if mobile_device?
-  end
+  #def prepare_for_mobile
+   # session[:mobile_param] = params[:mobile] if params[:mobile]
+    #request.format = :mobile if mobile_device?
+  #end
 
   protected
     # Returns the currently logged in user or nil if there isn't one
