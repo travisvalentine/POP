6da3e082fcc89e86776fdb463e2b97a453778c23
Travis Valentine <travis.valentine@livingsocial.com>
2012-03-16 16:23:04 -0400

    Fix password reset issue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,7 @@ POP::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
+  # GMail settings
   ActionMailer::Base.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -37,7 +37,7 @@ POP::Application.configure do
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
-
+  
   # Use SQL instead of Active Record's schema dumper when creating the test database.
   # This is necessary if your schema can't be completely dumped by the schema dumper,
   # like if you have constraints or database-specific column types
