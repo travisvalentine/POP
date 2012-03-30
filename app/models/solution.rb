5f60e6fae3cca84de47ce9e347f505c78a676886
Travis Valentine <tvalent2@gmualumni.org>
2012-03-30 15:32:22 -0400

    Add for solution

--- app/models/solution.rb
+++ app/models/solution.rb
@@ -1,7 +1,9 @@
 class Solution < ActiveRecord::Base
   validates :body, :presence => true
 
-  belongs_to :problem
+  belongs_to :problem, :foreign_key => "problem_id"
   belongs_to :user, :foreign_key => "user_id"
+
+  has_many :comments
 
 end
