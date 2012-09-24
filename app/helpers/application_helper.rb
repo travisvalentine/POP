2f35d4c5255b01de18c8d8f5dee1d2d3c7154c07
Travis Valentine <tvalent2@gmualumni.org>
2012-09-23 23:58:41 -0400

    Add active class to problems#index if fullpath is /problems

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def homepage?
     request.fullpath == root_path
   end
+
+  def index_active
+    if request.fullpath == "/problems"
+      content_tag(:li, link_to('Problems', problems_path), :class => "active")
+    else
+      content_tag(:li, link_to('Problems', problems_path))
+    end
+  end
 end
