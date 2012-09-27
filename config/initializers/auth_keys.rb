27e32f594336acbd9b25452c24f2b1b8549dd264
Travis Valentine <travis.valentine@livingsocial.com>
2012-09-27 00:36:21 -0400

    Modify Sunlight API variables

--- config/initializers/auth_keys.rb
+++ config/initializers/auth_keys.rb
@@ -3,4 +3,4 @@ TWITTER_KEY='CwTypnTmAGl0IqTpGX08g'
 TWITTER_SECRET='EKAz6ZOic7LkQaBXXsD8p68O0trdnundeshPxnHMN4'
 
 # Sunlight
-Sunlight::Base.api_key ='0ab78cd1a01f40628ff6e9bb0f13b703'
+SUNLIGHT_BASE_API ='0ab78cd1a01f40628ff6e9bb0f13b703'
