03357f11344848393edbd7ae542801d18abdb903
Travis Valentine <tvalent2@gmualumni.org>
2012-09-26 22:18:51 -0400

    Clean up session#create, separate oauth handling in own controller

---
+++ app/controllers/oauth_controller.rb
@@ -0,0 +1,25 @@
+class OauthController < ApplicationController
+
+  def create
+    auth = request.env["omniauth.auth"]
+    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"])
+
+    if user.nil?
+      user = User.create_with_omniauth(auth)
+      Profile.create_with_omniauth(user.id, auth)
+      login_from_oauth(user.id)
+      return
+    elsif user.present? and user.profile.present?
+      session[:user_id] = user.id
+      redirect_to problems_path, :notice => "Welcome back!"
+    end
+  end
+
+private
+
+  def login_from_oauth(user_id)
+    session[:user_id] = user_id
+    redirect_to problems_path, :notice => "Welcome to this!"
+  end
+
+end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,9 @@
 class SessionsController < ApplicationController
 
   def create
-    auth = request.env["omniauth.auth"]
-    if params[:email] and params[:password]
-      user = User.authenticate(params[:email], params[:password])
-    else
-      user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) ||
-             User.create_with_omniauth(auth)
-    end
+    user = User.authenticate(params[:email], params[:password])
+
     if user
-      if user.email.blank? and user.profile.blank?
-        Profile.create_with_omniauth(user.id, auth)
-      end
       session[:user_id] = user.id
       redirect_to problems_path, :notice => "Logged in successfully"
     else
@@ -24,7 +16,5 @@ class SessionsController < ApplicationController
     reset_session
     redirect_to root_path, :notice => "You successfully logged out"
   end
-
-private
 
 end
