2b72d6dedcf7ab45514467d4abbaf92813e52563
Travis Valentine <travis.valentine@livingsocial.com>
2012-09-27 00:32:56 -0400

    Add Sunlight API

--- config/initializers/auth_keys.rb
+++ config/initializers/auth_keys.rb
@@ -1,3 +1,6 @@
 # Twitter auth_keys
 TWITTER_KEY='CwTypnTmAGl0IqTpGX08g'
 TWITTER_SECRET='EKAz6ZOic7LkQaBXXsD8p68O0trdnundeshPxnHMN4'
+
+# Sunlight
+Sunlight::Base.api_key ='0ab78cd1a01f40628ff6e9bb0f13b703'
