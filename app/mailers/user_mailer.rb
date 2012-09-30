02ea580feb41c337abb48c02b6d36ed9c23a7c04
Travis Valentine <tvalent2@gmualumni.org>
2012-09-30 16:48:41 -0400

    Remove comment in mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,18 +3,19 @@ class UserMailer < ActionMailer::Base
   default :from => "Test <hungryopensource@gmail.com>",
           :return_path => "hungryopensource@gmail.com"
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.password_reset.subject
-  #
   def password_reset(user)
     @user = user
-    mail(:to => "#{@user.email}", :subject => "Check yourself and reset yourself")
+    mail(
+      :to => "#{@user.email}",
+      :subject => "Check yourself and reset yourself"
+    )
   end
 
   def registration_confirmation(user)
-    @user = user 
-    mail(:to => "#{@user.email}", :subject => "Thanks for Signing Up!")
-  end 
+    @user = user
+    mail(
+      :to => "#{@user.email}",
+      :subject => "Thanks for Signing Up!"
+    )
+  end
 end
