dfb60c821c25e55588c882e0bdb00e910de55a59
Mary Cutrali <mary.cutrali@gmail.com>
2012-09-30 15:27:48 -0400

    adding redis config initializer to app

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,9 +1,17 @@
 require 'resque'
-if Rails.env.production?
-  redis_url = ENV["REDISTOGO_URL"]
-  uri = URI.parse(redis_url)
-  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+# if Rails.env.production?
+#   redis_url = ENV["REDISTOGO_URL"]
+#   uri = URI.parse(redis_url)
+#   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+# else
+#   REDIS = Redis.new
+# end
+# Resque.redis = REDIS
+
+if Rails.env.development?
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  Resque.redis = Redis.new(:host => 'localhost', :port => '6379')
 else
-  REDIS = Redis.new
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 end
-Resque.redis = REDIS
