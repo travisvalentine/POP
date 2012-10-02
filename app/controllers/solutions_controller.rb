391db125040ac24540aea241f272cd1fa63b5201
Travis Valentine <tvalent2@gmualumni.org>
2012-10-02 00:54:05 -0400

    Solution form on profiles#show fixed

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -1,5 +1,5 @@
 class SolutionsController < ApplicationController
-  before_filter :find_problem, :except => :destroy
+  before_filter :find_problem, :except => [:destroy, :create]
   before_filter :authenticate, :only => :destroy
 
   def new
@@ -7,13 +7,14 @@ class SolutionsController < ApplicationController
   end
 
   def create
+    @problem = Problem.find(params[:solution][:problem_id])
     @solution = @problem.solutions.new(params[:solution])
     respond_to do |format|
       if @solution.save
         @solution.update_attributes(problem_id: @problem.id, user_id: current_user.id)
-        format.html { redirect_to(@problem, :alert => 'Solution was successfully created.') }
+        format.html { redirect_to(@problem, :notice => 'Solution was successfully created.') }
       else
-        format.html { render :action => "new" }
+        format.html { redirect_to(:back), :notice => "There was an error." }
       end
     end
 
@@ -27,7 +28,7 @@ class SolutionsController < ApplicationController
     @problem = current_user.problems.find(params[:problem_id])
     solution = @problem.solutions.find(params[:id])
     solution.destroy
-    redirect_to @problem, :alert => 'Solution deleted.'
+    redirect_to @problem, :notice => 'Solution deleted.'
   end
 
   private
