ea69068053db8c1047c1496b82273d1f4fbee43c
Travis Valentine <tvalent2@gmualumni.org>
2012-10-02 21:52:05 -0400

    Alter OmniAuth file

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  if Rails.env.production?
-    provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
-  else
-    provider :twitter, TWITTER_KEY, TWITTER_SECRET
-  end
+  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
 end
