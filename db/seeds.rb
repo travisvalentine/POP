1d27e3110bf1995e753c057acc6467b12b771812
Travis Valentine <tvalent2@gmualumni.org>
2012-10-08 00:16:37 -0400

    Add kaminari files. Style pagination

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,3 +32,23 @@ ISSUES = %w(
 ISSUES.each do |issue|
   Issue.create!(:name => issue)
 end
+
+if Rails.env.development?
+  test = FactoryGirl.create(:user, :email => "test@testing.com")
+  test_profile = FactoryGirl.create(:profile,
+                                    :name => "Testing Account",
+                                    :user_id => test.id)
+
+  user = FactoryGirl.create(:user)
+  user_profile = FactoryGirl.create(:profile,
+                                    :name => "Random Account",
+                                    :user_id => user.id)
+
+  20.times do
+    user.problems.create(:body => Faker::Lorem::paragraph(sentence_count=2))
+  end
+
+  user.problems.each do |p|
+    p.solutions.create(:body => Faker::Lorem::paragraph(sentence_count=2))
+  end
+end
